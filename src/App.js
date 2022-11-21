b32343aa72ac2892af388436c1d6b94d650a239d
Valerii Lozghachov <v.lozghachov@rolladengroup.de>
2022-11-21 12:12:59 +0100

    add mode toggle and new styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import { useState, useEffect } from "react";
 import Card from "./components/Card";
 
-const images = [
-  { src: "/images/artillery.png", matched: false },
+const easy = [
   { src: "/images/attack.png", matched: false },
   { src: "/images/defense.png", matched: false },
+  { src: "/images/power.png", matched: false },
+  { src: "/images/knowledge.png", matched: false },
+];
+const medium = [
+  { src: "/images/attack.png", matched: false },
+  { src: "/images/defense.png", matched: false },
+  { src: "/images/power.png", matched: false },
+  { src: "/images/knowledge.png", matched: false },
+  { src: "/images/fire.png", matched: false },
+  { src: "/images/water.png", matched: false },
+  { src: "/images/air.png", matched: false },
+  { src: "/images/earth.png", matched: false },
+  { src: "/images/necromancy.png", matched: false },
+];
+const hard = [
+  { src: "/images/attack.png", matched: false },
+  { src: "/images/defense.png", matched: false },
+  { src: "/images/power.png", matched: false },
+  { src: "/images/knowledge.png", matched: false },
+  { src: "/images/fire.png", matched: false },
+  { src: "/images/water.png", matched: false },
+  { src: "/images/air.png", matched: false },
+  { src: "/images/earth.png", matched: false },
+  { src: "/images/archery.png", matched: false },
+  { src: "/images/armorer.png", matched: false },
+  { src: "/images/artillery.png", matched: false },
+  { src: "/images/ballistics.png", matched: false },
   { src: "/images/diplomacy.png", matched: false },
   { src: "/images/gold.png", matched: false },
-  { src: "/images/power.png", matched: false },
-  { src: "/images/sorcery.png", matched: false },
   { src: "/images/leadership.png", matched: false },
-  //   { src: "/images/resistance.png", matched: false },
+  { src: "/images/logistics.png", matched: false },
 ];
 
 const App = () => {
@@ -19,11 +43,20 @@ const App = () => {
   const [pickFirst, setPickFirst] = useState(null);
   const [pickSecond, setPickSecond] = useState(null);
   const [disabled, setDisabled] = useState(false);
-
+  const [mode, setMode] = useState("easy");
   //   shuffle the board
   const randomizeCards = () => {
-    const arr = [...images, ...images].sort(() => Math.random() - 0.5).map((tile) => ({ ...tile, id: Math.random() }));
-    setCards(arr);
+    let arr = [];
+    if (mode === "easy") {
+      arr = [...easy, ...easy, ...easy, ...easy];
+    } else if (mode === "medium") {
+      arr = [...medium, ...medium, ...medium, ...medium];
+    } else {
+      arr = [...hard, ...hard, ...hard];
+    }
+
+    let init = arr.sort(() => Math.random() - 0.5).map((tile) => ({ ...tile, id: Math.random() }));
+    setCards(init);
     setTurns(0);
   };
   //   click
@@ -63,12 +96,43 @@ const App = () => {
   // init
   useEffect(() => {
     randomizeCards();
-  }, []);
+  }, [mode]);
+
   return (
     <div className='App'>
       <h1>Heroes 3 of Might and Magic memory game</h1>
-      <button onClick={randomizeCards}>New game</button>
-      <div className='board'>
+      <div className='control'>
+        <button onClick={randomizeCards}>New game</button>
+        <div className='mode'>
+          <span
+            className={mode === "easy" ? "active" : ""}
+            onClick={() => {
+              setMode("easy");
+              randomizeCards();
+            }}>
+            Easy
+          </span>
+          <span
+            className={mode === "medium" ? "active" : ""}
+            onClick={() => {
+              setMode("medium");
+              randomizeCards();
+            }}>
+            Med
+          </span>
+          <span
+            className={mode === "hard" ? "active" : ""}
+            onClick={() => {
+              setMode("hard");
+              randomizeCards();
+            }}>
+            Hard
+          </span>
+        </div>
+        <div className='turns'>Total turns: {turns}</div>
+      </div>
+
+      <div className={mode === "medium" ? "board medium" : mode === "hard" ? "board hard" : "board"}>
         {cards.map((card) => (
           <Card
             card={card}
@@ -79,7 +143,6 @@ const App = () => {
           />
         ))}
       </div>
-      <div className='turns'>Total turns: {turns}</div>
     </div>
   );
 };
